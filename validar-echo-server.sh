fea819365f184fc67caf32384e3156312a066b42
Luciano Martin Gamberale <lgamberale@fi.uba.ar>
2025-08-24 01:33:28 -0300

    :bug: do the echos into the container

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -5,12 +5,15 @@ service_name='nc-test_echo_server'
 alpine_image='alpine:3.22'
 
 message="[TEST ECHO SERVER] Message"
-command="echo '$message' | nc server 12345"
 
-reply=$(docker run --name=$service_name $alpine_image --network=$network_name --rm sh -c "$command")
+docker run -dit --name=$service_name --network=$network_name $alpine_image sh
 
+reply=$(docker exec $service_name sh -c "echo '$message' | nc server 12345")
 if [ "$reply" = "$message" ]; then
-  echo 'action: test_echo_server | result: success'
+    docker exec $service_name sh -c "echo 'action: test_echo_server | result: success'"
 else
-  echo 'action: test_echo_server | result: fail'
+    docker exec $service_name sh -c "echo 'action: test_echo_server | result: fail'"
 fi
+
+docker stop $service_name
+docker rm $service_name
