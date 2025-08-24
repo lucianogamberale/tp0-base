88e104faadf1aa3f7bcb14d4ba37b72269f888df
Luciano Martin Gamberale <lgamberale@fi.uba.ar>
2025-08-24 02:47:57 -0300

    :art: improve scripts

--- netcat-echo-sv-tester/netcat-echo-sv-test.sh
+++ netcat-echo-sv-tester/netcat-echo-sv-test.sh
@@ -2,9 +2,10 @@
 
 server_service_name='server'
 server_port=12345
+
 message="[TEST ECHO SERVER] Message"
 
-reply=$(echo "$message" | nc $server_service_name $server_port)
+reply=$(echo "$message" | nc "$server_service_name" "$server_port")
 if [ "$reply" = "$message" ]; then
     echo 'action: test_echo_server | result: success'
 else
--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-image_tag='netcat-echo-sv-tester:latest'
 network_name='tp0_testing_net'
+
+image_tag='netcat-echo-sv-tester:latest'
 script_name='netcat-echo-sv-test.sh'
 
 docker build --tag=$image_tag ./netcat-echo-sv-tester
-
-docker run \
-    --rm \
-    --network=$network_name \
-    "$image_tag" sh -c "./$script_name"
+docker run --rm --network=$network_name $image_tag sh -c "./$script_name"
